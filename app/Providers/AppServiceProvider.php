a6216f9e0f0ceb737bad35acd9fe8fe2f3aff4d2
conradoTon <conradoton@gmail.com>
2026-01-08 20:29:13 +0100

    Eliminar parches de rutas innecesarios para SiteGround

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,14 +28,7 @@ class AppServiceProvider extends ServiceProvider
         \App\Models\EstimateItem::observe(\App\Observers\EstimateItemObserver::class);
 
         if (app()->environment('production')) {
-            // Forzar HTTPS
             \Illuminate\Support\Facades\URL::forceScheme('https');
-
-            // Asegurar que Livewire usa la ruta correcta
-            \Livewire\Livewire::setUpdateRoute(function ($handle) {
-                return \Illuminate\Support\Facades\Route::post('/livewire/update', $handle)
-                    ->middleware(['web']);
-            });
         }
     }
 }
